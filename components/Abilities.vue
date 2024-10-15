70bdf7c14e9067acfe572c6d3966b74fd754e716
Theo Pierson <thheopierson@gmail.com>
2024-10-15 15:26:44 +0200

    feat: add hover effect to hero buttons and create Portfolio component

--- components/Abilities.vue
+++ components/Abilities.vue
@@ -1,40 +1,44 @@
 <template>
   <section class="grid place-items-center">
-    <h2 class="text-emerald-500 text-4xl text-center mt-2 font-semibold m-2">Mes Compétences</h2>
-    <div>
+    <h2 class="text-emerald-500 text-4xl text-center mt-2 font-semibold m-2 mb-10">Mes Compétences</h2>
+    <div class="md:grid grid-cols-2 w-3/4">
       <h3 class="text-2xl font-semibold m-1">Développement Frontend</h3>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="angular-icon.svg" alt="logo Angular" width="50px" /><span>Angular</span>
-        <p>Framework Frontend - Expérience: Depuis 2023</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="nuxt-icon.svg" alt="logo NuxtJs" width="50px" /><span>NuxtJs</span>
-        <p>Framework Frontend - Expérience: Depuis 2024</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="nextjs-icon.svg" alt="logo NextJS" width="50px" /><span>NextJs</span>
-        <p>Framework Frontend - Expérience: Depuis 2024</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="typescript-icon.svg" alt="logo TypeScript" width="50px" /><span>TypeScript</span>
-        <p>Langage de programation - Expérience: Depuis 2023</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="tailwindcss-icon.svg" alt="logo Tailwind" width="50px" /><span>Tailwind</span>
-        <p>Framework CSS - Expérience: Depuis 2024</p>
+      <div class="md:grid grid-cols-2" id="cards">
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="angular-icon.svg" alt="logo Angular" width="50px" /><span>Angular</span>
+          <p>Framework Frontend - Expérience: Depuis 2023</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="nuxt-icon.svg" alt="logo NuxtJs" width="50px" /><span>NuxtJs</span>
+          <p>Framework Frontend - Expérience: Depuis 2024</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="nextjs-icon.svg" alt="logo NextJS" width="50px" /><span>NextJs</span>
+          <p>Framework Frontend - Expérience: Depuis 2024</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="typescript-icon.svg" alt="logo TypeScript" width="50px" /><span>TypeScript</span>
+          <p>Langage de programation - Expérience: Depuis 2023</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="tailwindcss-icon.svg" alt="logo Tailwind" width="50px" /><span>Tailwind</span>
+          <p>Framework CSS - Expérience: Depuis 2024</p>
+        </div>
       </div>
       <h3 class="text-2xl font-semibold m-1 w-5/6">Développement Backend et base de données</h3>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="java.svg" alt="logo java" width="50px" /><span>Java</span>
-        <p>Langage de programation - Expérience: Depuis 2023</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="spring-icon.svg" alt="logo Spring" width="50px" /><span>Spring</span>
-        <p>Framework Backend - Expérience: Depuis 2023</p>
-      </div>
-      <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
-        <NuxtImg src="postgresql.svg" alt="logo Spring" width="50px" /><span>PostgreSQL</span>
-        <p>Base de données - Expérience: Depuis 2023</p>
+      <div class="md:grid grid-cols-2" id="cards">
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="java.svg" alt="logo java" width="50px" /><span>Java</span>
+          <p>Langage de programation - Expérience: Depuis 2023</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="spring-icon.svg" alt="logo Spring" width="50px" /><span>Spring</span>
+          <p>Framework Backend - Expérience: Depuis 2023</p>
+        </div>
+        <div class="grid place-items-center border-b border-neutral-400 p-4 w-3/4 mx-auto">
+          <NuxtImg src="postgresql.svg" alt="logo Spring" width="50px" /><span>PostgreSQL</span>
+          <p>Base de données - Expérience: Depuis 2023</p>
+        </div>
       </div>
     </div>
   </section>
--- components/PersonnalInfo.vue
+++ components/PersonnalInfo.vue
@@ -3,7 +3,7 @@
     <h1 class="text-5xl mt-24 text-center">
       Développeur Web - <span class="font-CourierPrime text-6xl font-bold">Freelance</span>
     </h1>
-    <h2 class="text-4xl text-center mt-2">Developpeur Angular | Vue | Springboot à Lille</h2>
+    <h2 class="text-4xl text-center mt-2">Développeur Angular | Vue | Springboot à Lille</h2>
 
     <h3 class="text-4xl flex justify-center items-center mt-2">
       <Icon name="majesticons:map-marker" />Lille | Lestrem
@@ -11,13 +11,40 @@
     <p class="font-CourierPrime text-xl mt-10 text-center">Création d'application web, site vitrine, landing page.</p>
     <p class="font-CourierPrime text-xl text-center">Prêt à vous aider à résoudre votre problème !</p>
     <div class="flex m-2">
-      <button class="m-2 p-1 border-b-2 border-emerald-800 font-CourierPrime hover:bg-emerald-800">
-        Voir mes créations</button
-      ><button class="m-2 p-1 border-b-2 border-emerald-800 font-CourierPrime">Me contacter</button>
+      <button class="btn-hover m-2 p-1 border-b-2 border-emerald-800 font-CourierPrime">Voir mes créations</button>
+      <button class="btn-hover m-2 p-1 border-b-2 border-emerald-800 font-CourierPrime">Me contacter</button>
     </div>
   </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.btn-hover {
+  position: relative;
+  overflow: hidden;
+  background: transparent;
+  z-index: 1;
+  transition: color 0.4s ease;
+}
+
+.btn-hover::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background-color: #04784e; /* Couleur vert émeraude */
+  z-index: -1;
+  transition: left 0.4s ease;
+}
+
+.btn-hover:hover::before {
+  left: 0;
+}
+
+.btn-hover:hover {
+  color: white;
+}
+</style>
