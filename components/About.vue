e7c570a205af1dc13dfafedb2c0e0273c0b31290
Theo Pierson <thheopierson@gmail.com>
2025-03-13 14:07:09 +0100

    feat: made website responsive

--- components/About.vue
+++ components/About.vue
@@ -1,43 +1,68 @@
 <template>
   <h2 :class="{ 'fade-in': isVisible }" class="text-5xl mt-20">Mon profil</h2>
-
-  <section id="about" class="grid md:flex place-items-center md:w-screen">
+  <section class="grid gap-4 md:w-auto md:flex justify-around">
     <NuxtImg
       data-aos="fade-left"
       id="profilePic"
-      class="m-2 md:hidden rounded-xl"
+      class="m-2 md:hidden rounded-xl ml-4"
       src="profilepic.jpeg"
       alt="photo de Theo Pierson Dévelppeur Web à Lestrem/Lille"
       width="300px"
     />
     <!--TODO Faire 3 colonnes au lieu de 3 lignes avec les decriptions + Ajouter des animations comme pour les logos de
     stack -->
-    <div class="p-4 md:p-10 md:w-4/5" id="content" data-aos="fade-right">
-      <h3 class="font-medium text-xl">Parcours scolaire</h3>
-      <p class="leading-tight mb-8">
-        Après une prépa Art et Design et une immersion dans l'univers du tatouage, , j'ai décidé de me réorienter vers
-        un domaine qui allie ma passion pour l'informatique et mon goût pour l'innovation et la créativité : le
-        développement web. Afin d'acquérir rapidement les compétences nécessaires, j'ai opté pour des formations
-        accélérées et une alternance, ce qui m'a permis d'obtenir en seulement deux ans un titre de Concepteur
-        Développeur d'Applications (CDA). Aujourd'hui, fort de cette expérience et de ma licence, je suis prêt à relever
-        de nouveaux défis dans le monde du développement, en alliant créativité, technique et innovation.
-      </p>
-      <h3 class="font-medium text-xl">Début de parcours professionnel</h3>
-      <p class="mt-1 mb-8 leading-tight">
-        Mes débuts en développement ont été marqués par des projets explorant diverses technologies front et back-end.
-        Aujourd'hui, je me spécialise dans des outils modernes comme Vue.js, NuxtJs et Angular pour créer des interfaces
-        utilisateur fluides et engageantes. En parallèle, je développe des backends solides en utilisant Java Spring
-        Boot, souvent conteneurisés avec Docker pour une intégration efficace.
-      </p>
-      <h3 class="font-medium text-xl">Expériences professionnelles</h3>
-
-      <p class="leading-tight">
-        En tant que développeur web, j'ai acquis une expérience significative au fil des projets, toujours animé par la
-        volonté de répondre aux besoins spécifiques de mes clients et de leur offrir des solutions sur mesure. Mon
-        engagement réside dans la création de plateformes performantes et une poussée de ma créativité au maximum, afin
-        de permettre à mes clients d'améliorer leur présence en ligne, d'optimiser leurs processus ou de simplifier la
-        gestion de leurs activités quotidiennes.
-      </p>
+    <div class="flip-card md:m-8">
+      <div class="flip-card-inner">
+        <div class="flip-card-front grid place-items-center text-9xl border-2 border-black rounded-lg">
+          <Icon name="game-icons:spell-book" />
+          <h3 class="font-semibold text-xl">Parcours scolaire</h3>
+        </div>
+        <div class="flip-card-back border-2 border-black rounded-lg">
+          <p class="title">Parcours scolaire</p>
+          <p class="text-center p-4">
+            Issus d'une prépa Art et Design et du tatouage, j'ai décidé de me professionnaliser dans un domaine qui
+            allie ma passion pour l'informatique et mon goût pour l'innovation et la créativité : le développement web.
+            Afin d'acquérir rapidement les compétences nécessaires, j'ai opté pour des formations accélérées et une
+            alternance, ce qui m'a permis d'obtenir en seulement deux ans un titre de Concepteur Développeur
+            d'Applications (CDA).
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flip-card md:m-8">
+      <div class="flip-card-inner">
+        <div class="flip-card-front grid place-items-center text-9xl border-2 border-black rounded-lg">
+          <Icon name="hugeicons:dumbbell-03" />
+          <h3 class="font-semibold text-xl">Début de parcours professionel</h3>
+        </div>
+        <div class="flip-card-back border-2 border-black rounded-lg">
+          <p class="title">Début de parcours professionel</p>
+          <p class="text-center p-4">
+            Mes débuts en développement ont été marqués par des projets explorant diverses technologies front et
+            back-end. Aujourd'hui, je me spécialise dans des outils modernes comme Vue.js, NuxtJs et Angular pour créer
+            des interfaces utilisateur fluides et engageantes. En parallèle, je développe des backends solides en
+            utilisant Java Spring Boot, souvent conteneurisés avec Docker pour une intégration efficace.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="flip-card md:m-8">
+      <div class="flip-card-inner">
+        <div class="flip-card-front grid place-items-center text-9xl border-2 border-black rounded-lg">
+          <Icon name="game-icons:tie" />
+          <h3 class="font-semibold text-xl">Expérience professionlle</h3>
+        </div>
+        <div class="flip-card-back border-2 border-black rounded-lg">
+          <p class="title">Expérience professionlle</p>
+          <p class="text-center p-4">
+            En tant que développeur web, j'ai acquis une expérience significative au fil des projets, toujours animé par
+            la volonté de répondre aux besoins spécifiques de mes clients et de leur offrir des solutions sur mesure.
+            Mon engagement réside dans la création de plateformes performantes et une poussée de ma créativité au
+            maximum, afin de permettre à mes clients d'améliorer leur présence en ligne, d'optimiser leurs processus ou
+            de simplifier la gestion de leurs activités quotidiennes.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -68,3 +93,55 @@ const scrollToSection = (sectionId: string): void => {
   }
 };
 </script>
+<style scoped>
+/* From Uiverse.io by joe-watson-sbf */
+.flip-card {
+  background-color: transparent;
+  width: 26rem;
+  height: 32rem;
+  perspective: 1000px;
+  font-family: sans-serif;
+}
+@media (max-width: 768px) {
+  .flip-card {
+    width: 21rem /* 32px */;
+    height: 23rem;
+  }
+}
+
+.title {
+  font-size: 1.5em;
+  font-weight: 900;
+  text-align: center;
+  margin: 0;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+</style>
