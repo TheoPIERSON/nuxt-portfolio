771007f315fa16eecbd4a627fd13ace72b354498
Theo Pierson <thheopierson@gmail.com>
2025-03-14 17:20:36 +0100

    feat: switch social links img to icon

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,23 +1,19 @@
 <template>
-  <section>
-    <ul class="flex md:inline mr-36 mt-0 md:mt-32 w-full md:w-10 justify-around">
-      <li class="mt-10 p-1 transform transition-transform duration-300 hover:rotate-45">
-        <a href="https://www.instagram.com/thistheop?igsh=MWR3ZWd3YXdvZGZpbg%3D%3D&utm_source=qr">
-          <NuxtImg class="w-10" width="100px" src="../public/Logo Instagram.png" alt=""
-        /></a>
-      </li>
-      <li class="mt-10 p-1 transform transition-transform duration-300 hover:rotate-45">
-        <a href="https://www.linkedin.com/in/theo-pierson-5bab87216/">
-          <NuxtImg class="w-10" width="100px" src="../public/Logo LinkedIn.png" alt=""
-        /></a>
-      </li>
-      <li class="mt-10 p-1 transform transition-transform duration-300 hover:rotate-45">
-        <a href="https://github.com/TheoPIERSON">
-          <NuxtImg class="w-10" width="100px" src="../public/Logo GitHub.svg" alt=""
-        /></a>
-      </li>
-    </ul>
-  </section>
+  <ul class="flex md:inline mr-36 mt-0 md:mt-32 w-full md:w-10 justify-around">
+    <li class="mt-4 p-1 transform transition-transform duration-300 hover:rotate-45">
+      <a href="https://www.instagram.com/thistheop?igsh=MWR3ZWd3YXdvZGZpbg%3D%3D&utm_source=qr">
+        <Icon name="fontisto:instagram" class="w-8" size="100px"
+      /></a>
+    </li>
+    <li class="mt-4 p-1 transform transition-transform duration-300 hover:rotate-45">
+      <a href="https://www.linkedin.com/in/theo-pierson-5bab87216/">
+        <Icon name="fontisto:linkedin" class="w-8" size="100px"
+      /></a>
+    </li>
+    <li class="mt-4 p-1 transform transition-transform duration-300 hover:rotate-45">
+      <a href="https://github.com/TheoPIERSON"> <Icon name="fontisto:github" class="w-8" size="100px" /></a>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts"></script>
