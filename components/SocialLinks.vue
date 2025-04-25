cba8b8fdd2dcb6989c9952b5ad718c8cfbab62bf
Theo Pierson <thheopierson@gmail.com>
2025-04-25 12:33:15 +0200

    feat: change image path

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="flex md:inline mr-36 mt-0 md:mt-32 w-full md:w-10 justify-around">
+  <ul class="flex md:inline mr-28 mt-0 md:mt-32 w-full md:w-10 justify-around">
     <li class="mt-4 p-1 transform transition-transform duration-300 hover:rotate-45">
       <a href="https://www.instagram.com/thistheop?igsh=MWR3ZWd3YXdvZGZpbg%3D%3D&utm_source=qr">
         <Icon name="fontisto:instagram" class="w-8" size="100px"
